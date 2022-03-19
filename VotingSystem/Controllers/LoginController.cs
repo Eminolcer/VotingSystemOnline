769e8869ee9d76f5eafa31f746039406786a584f
Emin Ölcer <Eminolcer@gmail.com>
2022-03-19 18:20:54 +0100

    Login Test

---
+++ VotingSystem/Controllers/LoginController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VotingSystem.Models;
+using VotingSystem.Services;
+
+namespace VotingSystem.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(UserModel userModel)
+        {
+            SecurityService securityService = new SecurityService();
+         
+            if (securityService.IsValid(userModel)) 
+            {
+                return View("LoginSucces", userModel);
+            } else
+            {
+                return View("LoginFailure", userModel);
+            }
+         
+        }
+    }
+}
